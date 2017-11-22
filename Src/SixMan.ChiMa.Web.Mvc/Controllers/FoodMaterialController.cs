1a4347eb56fbd442de71c80d62dc58d9da3364e0
szdls <szdls@LAPTOP-MJVJ39DS>
2017-11-22 10:54:44 +0800

    导入前段部分

--- Src/SixMan.ChiMa.Web.Mvc/Controllers/FoodMaterialController.cs
+++ Src/SixMan.ChiMa.Web.Mvc/Controllers/FoodMaterialController.cs
@@ -1,5 +1,8 @@
 ﻿using Abp.Application.Services.Dto;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
 using SixMan.ChiMa.Application.Base;
 using SixMan.ChiMa.Application.Food;
 using SixMan.ChiMa.Controllers;
@@ -7,7 +10,9 @@ using SixMan.ChiMa.Web.Models;
 using SixMan.ChiMa.Web.Models.Food;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -17,10 +22,13 @@ namespace SixMan.ChiMa.Web.Controllers
         : ChiMaControllerBase
     {
         private readonly IFoodMaterialAppService _appService;
+        private IHostingEnvironment _hostingEnvironment;
 
-        public FoodMaterialController(IFoodMaterialAppService appService)
+
+        public FoodMaterialController(IFoodMaterialAppService appService, IHostingEnvironment hostingEnvironment)
         {
             _appService = appService;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         public ActionResult Index()
@@ -51,6 +59,50 @@ namespace SixMan.ChiMa.Web.Controllers
                 Current = fmc,
             };
             return View("_EditFoodMaterialModal", vm);
+        }
+
+        [HttpPost]
+        public IActionResult Import(IFormFile excelfile)
+        {
+            string sWebRootFolder = _hostingEnvironment.WebRootPath;
+            string sFileName = $"{Guid.NewGuid()}.xlsx";
+            FileInfo file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
+            try
+            {
+                using (FileStream fs = new FileStream(file.ToString(), FileMode.Create))
+                {
+                    excelfile.CopyTo(fs);
+                    fs.Flush();
+                }
+                using (ExcelPackage package = new ExcelPackage(file))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                    int rowCount = worksheet.Dimension.Rows;
+                    int ColCount = worksheet.Dimension.Columns;
+                    bool bHeaderRow = true;
+                    for (int row = 1; row <= 4; row++)
+                    {
+                        for (int col = 1; col <= ColCount; col++)
+                        {
+                            if (bHeaderRow)
+                            {
+                                sb.Append(worksheet.Cells[row, col].Value?.ToString() + "\t");
+                            }
+                            else
+                            {
+                                sb.Append(worksheet.Cells[row, col].Value?.ToString() + "\t");
+                            }
+                        }
+                        sb.Append(Environment.NewLine);
+                    }
+                    return Content(sb.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
         }
     }
 }
