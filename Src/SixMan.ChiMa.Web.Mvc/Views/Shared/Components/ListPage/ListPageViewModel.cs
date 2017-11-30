366453f52ae5d818e225bc2ac1e25dc49cbbdab5
szdls <szdls@LAPTOP-MJVJ39DS>
2017-11-30 13:43:13 +0800

    View的组件化重构完成

---
+++ Src/SixMan.ChiMa.Web.Mvc/Views/Shared/Components/ListPage/ListPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Abp.Localization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace SixMan.ChiMa.Web.Views.Shared.Components.ListPage
+{
+    public class ListPageViewModel
+    { 
+        public ModelMetadata Meta { get; set; }
+    }
+}
