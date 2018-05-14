a4d2cd4564e07bbb8ea7d90bb5ffb444ddb48541
szdlsoft <szdlsoft@126.com>
2018-05-14 13:28:52 +0800

    部署test ok

--- Src/SixMan.ChiMa.Domain/AppVersionHelper.cs
+++ Src/SixMan.ChiMa.Domain/AppVersionHelper.cs
@@ -82,7 +82,7 @@ namespace SixMan.ChiMa.Domain
         /// Gets current version of the application.
         /// It's also shown in the web page.
         /// </summary>
-        public const string Version = "1.4.1";
+        public const string Version = "1.4.3";
 
         /// <summary>
         /// Gets release (last build) date of the application.
